afbe4b4e3838443c65aaad3e4d8d4b91c4951da5
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-11 16:18:53 +0530

    blogs-home-page-ready

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,4 +1,5 @@
-import logo from '../Assets/icon.svg'
+import logo from '../Assets/icon.svg';
+import code from '../Assets/code.svg';
 
 export default function Blogs(){
   return (
@@ -14,8 +15,20 @@ export default function Blogs(){
       </div>
       <div className = "nav-items">
         <ul>
-          <li className = "nav-item" id = "home"><a href = "#hero">Go to Home</a></li>
+          <li className = "nav-item" id = "home"><a href = "#hero" className = "back-botton"><span className = "left-arrow">←</span>Go to Home</a></li>
         </ul>
+      </div>
+    </div>
+    <div className = "blogs-wrapper">
+      <div className= "blogs-welcome-wrapper">
+        <div className = "blogs-image-wrapper">
+          <img src = {code} className = "blogs-image"/>
+        </div>
+        <div className = "blogs-text-wrapper">
+          <h1 className = "h1-blogs">Hola, Welcome to Avinash's Blogs !</h1>
+          <h2>I do write blogs about diffrent technologies and also differents opportunities.</h2>
+          <h3>Scroll down to read my latest blog <span className = "down-arrow">↓</span></h3>
+        </div>
       </div>
     </div>
     </>
